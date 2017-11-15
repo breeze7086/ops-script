8f5e026781cc24c83158d41c7edcf987aa719571
马营 <may@uc888.cn>
2017-11-15 10:42:08 +0800

    1,get different 'newest_file' cause of its character feature 2,if 'newest_file' greater than 500MB ,then refresh

--- python/rotate_logs.py
+++ python/rotate_logs.py
@@ -26,39 +26,56 @@ def rotate_logs():
                 if filename[:4] == 'game':
                     restr = re.compile('\d{4,}')
                     file_date = re.search(restr, fullpath_file).group()
+                    file_prefix = 'game'
                 elif filename[:4] == 'nlog':
                     file_date = string.split(''.join(string.split(filename, '-')[2:5]), '.')[0]
+                    file_prefix = 'nlog'
                 elif filename[:5] == 'lobby':
                     restr = re.compile('\d{4,}')
                     file_date = re.search(restr, fullpath_file).group()
+                    file_prefix = 'lobby'
                 if file_date:
                     if int(current_date[-4:-2]) - int(file_date[-4:-2]) == 1:
                         if MONTH_DEADLINE[int(current_month)] - int(file_date[-2:]) + int(current_date[-2:]) > ROTATE_DAYS:
-                            newest_file = get_dir_newest_file(fullpath_file)
+                            newest_file = get_dir_newest_file(fullpath_file, file_prefix)
+                            if os.path.getsize(newest_file) >= 524288000:
+                                cmd = 'echo "" > ' + newest_file
+                                os.system(cmd)
                             if newest_file != fullpath_file:
                                 print(fullpath_file)
                                 os.remove(fullpath_file)
                     else:
                         if int(current_date) - int(file_date) > ROTATE_DAYS:
-                            newest_file = get_dir_newest_file(fullpath_file)
+                            newest_file = get_dir_newest_file(fullpath_file, file_prefix)
+                            if os.path.getsize(newest_file) >= 524288000:
+                                cmd = 'echo "" > ' + newest_file
+                                os.system(cmd)
                             if newest_file != fullpath_file:
                                 print(fullpath_file)
                                 os.remove(fullpath_file)
             elif string.split(fullpath_file, '.')[-1] == 'txt':
                 if '-' in filename:
                     file_date = string.split(''.join(string.split(filename, '-')[0:]), '.')[0]
+                    file_suffix = '.txt'
                 else:
                     file_date = string.split(filename, '.')[-2]
+                    file_suffix = '.txt'
                 if file_date:
                     if int(current_date[-4:-2]) - int(file_date[-4:-2]) == 1:
                         if MONTH_DEADLINE[int(current_month)] - int(file_date[-2:]) + int(current_date[-2:]) > ROTATE_DAYS:
-                            newest_file = get_dir_newest_file(fullpath_file)
+                            newest_file = get_dir_newest_file(fullpath_file, file_suffix)
+                            if os.path.getsize(newest_file) >= 524288000:
+                                cmd = 'echo "" > ' + newest_file
+                                os.system(cmd)
                             if newest_file != fullpath_file:
                                 print(fullpath_file)
                                 os.remove(fullpath_file)
                     else:
                         if int(current_date) - int(file_date) > ROTATE_DAYS:
-                            newest_file = get_dir_newest_file(fullpath_file)
+                            newest_file = get_dir_newest_file(fullpath_file, file_suffix)
+                            if os.path.getsize(newest_file) >= 524288000:
+                                cmd = 'echo "" > ' + newest_file
+                                os.system(cmd)
                             if newest_file != fullpath_file:
                                 print(fullpath_file)
                                 os.remove(fullpath_file)
@@ -66,15 +83,16 @@ def rotate_logs():
 def get_current_date():
     return time.strftime("%Y%m%d", time.localtime())
 
-def get_dir_newest_file(fullpath_filename):
+def get_dir_newest_file(fullpath_filename, match_str):
     newest_file_time = 0
     path = os.path.dirname(fullpath_filename) + '/'
     for filename in os.listdir(os.path.dirname(fullpath_filename)):
-        file_ctime = os.stat(path + filename).st_ctime
-        if newest_file_time < file_ctime:
-            newest_file_time = file_ctime
-            newest_file = path + filename
-    return newest_file
+        if match_str in filename:
+            file_ctime = os.stat(path + filename).st_ctime
+            if newest_file_time < file_ctime:
+                newest_file_time = file_ctime
+                newest_file = path + filename
+                return newest_file
 
 if __name__ == '__main__':
     rotate_logs()
