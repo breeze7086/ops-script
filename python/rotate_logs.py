e877817867486b5cb95fa77809e3b80ff3efb767
UC888\may <may@uc888.cn>
2018-02-05 14:48:53 +0800

    check True if newest_file is the only specific file in the directory

--- python/rotate_logs.py
+++ python/rotate_logs.py
@@ -34,11 +34,15 @@ def rotate_logs():
                     restr = re.compile('\d{4,}')
                     file_date = re.search(restr, fullpath_file).group()
                     file_prefix = 'lobby'
+                elif filename[:8] == 'platform':
+                    restr = re.compile('\d{4,}')
+                    file_date = re.search(restr, fullpath_file).group()
+                    file_prefix = 'platform'
                 if file_date:
                     if int(current_date[-4:-2]) - int(file_date[-4:-2]) == 1:
                         if MONTH_DEADLINE[int(file_date[-4:-2]) - 1] - int(file_date[-2:]) + int(current_date[-2:]) > ROTATE_DAYS:
                             newest_file = get_dir_newest_file(fullpath_file, file_prefix)
-                            if os.path.getsize(newest_file) >= 524288000:
+                            if check_dir_newest_file_only(fullpath_file, newest_file) == True and os.path.getsize(newest_file) >= 524288000:
                                 cmd = 'echo "" > ' + newest_file
                                 os.system(cmd)
                             if newest_file != fullpath_file:
@@ -47,7 +51,7 @@ def rotate_logs():
                     else:
                         if int(current_date) - int(file_date) > ROTATE_DAYS:
                             newest_file = get_dir_newest_file(fullpath_file, file_prefix)
-                            if os.path.getsize(newest_file) >= 524288000:
+                            if check_dir_newest_file_only(fullpath_file, newest_file) == True and os.path.getsize(newest_file) >= 524288000:
                                 cmd = 'echo "" > ' + newest_file
                                 os.system(cmd)
                             if newest_file != fullpath_file:
@@ -64,7 +68,7 @@ def rotate_logs():
                     if int(current_date[-4:-2]) - int(file_date[-4:-2]) == 1:
                         if MONTH_DEADLINE[int(file_date[-4:-2]) - 1] - int(file_date[-2:]) + int(current_date[-2:]) > ROTATE_DAYS:
                             newest_file = get_dir_newest_file(fullpath_file, file_suffix)
-                            if os.path.getsize(newest_file) >= 524288000:
+                            if check_dir_newest_file_only(fullpath_file, newest_file) == True and os.path.getsize(newest_file) >= 524288000:
                                 cmd = 'echo "" > ' + newest_file
                                 os.system(cmd)
                             if newest_file != fullpath_file:
@@ -73,7 +77,7 @@ def rotate_logs():
                     else:
                         if int(current_date) - int(file_date) > ROTATE_DAYS:
                             newest_file = get_dir_newest_file(fullpath_file, file_suffix)
-                            if os.path.getsize(newest_file) >= 524288000:
+                            if check_dir_newest_file_only(fullpath_file, newest_file) == True and os.path.getsize(newest_file) >= 524288000:
                                 cmd = 'echo "" > ' + newest_file
                                 os.system(cmd)
                             if newest_file != fullpath_file:
@@ -93,6 +97,18 @@ def get_dir_newest_file(fullpath_filename, match_str):
                 newest_file_time = file_ctime
                 newest_file = path + filename
     return newest_file
+
+def check_dir_newest_file_only(fullpath_filename, newest_file):
+    count = 0
+    newest_file_prefix = '_'.join(newest_file.split('/')[-1].split('_')[0:2])
+    path = os.path.dirname(fullpath_filename) + '/'
+    for filename in os.listdir(os.path.dirname(fullpath_filename)):
+        if os.path.isfile(filename) == True:
+            if newest_file_prefix in filename:
+                count = count + 1
+            if count >= 2:
+                return False
+    return True
 
 if __name__ == '__main__':
     rotate_logs()
